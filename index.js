85c922e065b475537bdcc9b60fb78cae66c0169c
Yurii Kuliasov <mr.awesome@i.ua>
2021-10-28 19:54:37 +0300

    +

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
-import refs from "./refs.js";
-const { body, startBtn, stopBtn } = refs;
 const colors = [
-  "#FFFFFF",
-  "#2196F3",
-  "#4CAF50",
-  "#FF9800",
-  "#009688",
-  "#795548",
-];
-startBtn.addEventListener("click", changeBgc);
-stopBtn.addEventListener("click", stopChangebgc);
-
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-let interval = null;
-
-function changeBgc() {
-  interval = setInterval(() => {
-    body.style.backgroundColor =
-      colors[randomIntegerFromInterval(0, colors.length - 1)];
-  }, 1000);
-  startBtn.disabled = true;
-}
-
-function stopChangebgc() {
-  clearInterval(interval);
-  stopBtn.disabled = false;
-}
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+  ];
+  
+  const refs = {
+      body: document.body,
+      btnStart: document.querySelector('button[data-action = "start"]'),
+      btnStop: document.querySelector('button[data-action = "stop"]'),
+  }
+  
+  const INTERVAL_DELAY = 1000;
+  let intervalId = null;
+  
+  refs.btnStart.addEventListener('click', changeColor);
+  refs.btnStop.addEventListener('click', onBtnStop);
+  
+  const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+  
+  function changeColor() {
+      intervalId = setInterval(() => {
+          refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+      }, INTERVAL_DELAY);
+      refs.btnStart.disabled = true;
+  };
+  
+  function onBtnStop() {
+      clearInterval(intervalId);
+      refs.btnStart.disabled = false;
+  }
--- refs.js
+++
@@ -1,5 +0,0 @@
-export default {
-    body: document.body,
-    startBtn: document.querySelector('[data-action="start"]'),
-    stopBtn: document.querySelector('[data-action="stop"]'),
-  };
